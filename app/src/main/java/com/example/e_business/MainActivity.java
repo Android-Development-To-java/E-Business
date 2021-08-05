c6437816d30893dcefbc788e8e3490a197ca100a
DARK LIFE <onlyanamhoque@gmail.com>
2021-08-06 01:21:31 +0600

    Add somethings AnaMul

--- app/src/main/java/com/example/e_business/MainActivity.java
+++ app/src/main/java/com/example/e_business/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case R.id.account:
                         mySetFragment(new AccountFragment());
+                        break;
 
                 }
 
