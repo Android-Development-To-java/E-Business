d03d1d93bd3227b372499c3412092fa302de57a7
ParthaMajumder <parthamajumder9810@gmail.com>
2021-08-05 23:00:40 +0600

    hi mr

--- app/src/main/java/com/example/e_business/MainActivity.java
+++ app/src/main/java/com/example/e_business/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.e_business;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.buttom_nav);
+        frameLayout = findViewById(R.id.frame_layout);
     }
 }
