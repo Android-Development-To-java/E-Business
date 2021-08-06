97e0406b9cb118879cf0a5b1789dbafe383bdd57
ParthaMajumder <parthamajumder9810@gmail.com>
2021-08-06 20:57:26 +0600

    hi mr

--- app/src/main/java/com/example/e_business/utility/ui/HomeFragment.java
+++ app/src/main/java/com/example/e_business/utility/ui/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import androidx.fragment.app.Fragment;
 
@@ -17,13 +18,13 @@ import com.smarteist.autoimageslider.SliderView;
 
 public class HomeFragment extends Fragment {
 
+    GridView gridView;
      SliderView sliderViewS;
 
 //     int [] imgS = getResources().getIntArray(R.array.imgAll);
 
 
      int [] img =  {R.drawable.image2,R.drawable.image1,R.drawable.image2,R.drawable.image1};
-
 
 
 
@@ -43,6 +44,7 @@ public class HomeFragment extends Fragment {
         View views = inflater.inflate(R.layout.fragment_home, container, false);
 
         sliderViewS = views.findViewById(R.id.image_slider);
+        gridView = views.findViewById(R.id.gridView);
 
         SlidingAdapter mySliderAdapter = new SlidingAdapter(img,getContext());
 
@@ -55,6 +57,7 @@ public class HomeFragment extends Fragment {
         sliderViewS.setIndicatorUnselectedColor(Color.GRAY);
         sliderViewS.setScrollTimeInSec(4); //set scroll delay in seconds :
         sliderViewS.startAutoCycle();
+
 
 
 
